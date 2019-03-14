60e3c60d15dac1399343f79c2c66720f9a2ffdc0
Julian Builes Cajiaoju <Julian9129@hotmail.com>
2019-03-14 16:32:07 -0500

    Fethc con la consulta al api que retorna los cliente

--- src/clients/containers/clients.js
+++ src/clients/containers/clients.js
@@ -1,26 +1,27 @@
-import React, {Component} from 'react'
-import GridClients from '../components/grid-clients';
-import Routes from '../../utils/containers/routes'
+import React, { Component } from "react";
+import GridClients from "../components/grid-clients";
+import Routes from "../../utils/containers/routes";
 class Clients extends Component {
-    state = {
-        clients : [],
-        routes: [{name:"Clientes" , active:true}],
-    }
+  state = {
+    clients: null,
+    routes: [{ name: "Clientes", active: true }]
+  };
 
-    componentWillMount(){
-        fetch('http://localhost:8080/clients')
-            .then(response => response.json())
-            .then(clients => this.setState({clients}) )
-    }
+  componentWillMount() {
+    fetch("http://localhost:8080/clients")
+      .then(response => response.json())
+      .then(clients => this.setState({ clients }));
+  }
 
-    render(){
-        return(
-            <div>
-                <Routes  name={this.state.routes} />
-                <GridClients clientes = {this.state.clients} />
-            </div>
-        )
-    }
+  render() {
+    console.log(this.state.clients);
+    return (
+      <div>
+        <Routes name={this.state.routes} />
+        <GridClients clientes={this.state.clients} />
+      </div>
+    );
+  }
 }
 
-export default Clients
+export default Clients;
