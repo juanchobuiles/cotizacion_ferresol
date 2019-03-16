cf8c3fe24139032a5c8092ad40d0b8bdf77d6e06
Julian Builes Cajiaoju <Julian9129@hotmail.com>
2019-03-16 10:41:31 -0500

    Adicion al forntend para que funcione el guardado de elemento

--- src/selections/components/grid-new-mainproduct.js
+++ src/selections/components/grid-new-mainproduct.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import { BootstrapTable,TableHeaderColumn } from 'react-bootstrap-table'
-const GridNewMainProduct = (props) => {
- 
-    return (
-        <BootstrapTable data={props.data} >
-            <TableHeaderColumn dataField='id' isKey={true}>ID</TableHeaderColumn>
-            <TableHeaderColumn dataField='tela'>Tela</TableHeaderColumn>
-            <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
-        </BootstrapTable>
-    )
-}
+import React from "react";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+const GridNewMainProduct = props => {
+  return (
+    <BootstrapTable data={props.tela}>
+      <TableHeaderColumn dataField="id" isKey={true}>
+        ID
+      </TableHeaderColumn>
+      <TableHeaderColumn dataField="name_selection">Tela</TableHeaderColumn>
+      <TableHeaderColumn dataField="price">Product Price</TableHeaderColumn>
+    </BootstrapTable>
+  );
+};
 
-export default GridNewMainProduct
+export default GridNewMainProduct;
--- src/selections/containers/new-seleccion.js
+++ src/selections/containers/new-seleccion.js
@@ -1,143 +1,185 @@
-import React, { Component } from 'react'
-import Routes from '../../utils/containers/routes';
-import { Card, Button, Collapse, InputGroup, FormControl } from 'react-bootstrap';
-import GridNewMainProduct from '../components/grid-new-mainproduct'
-import Select from 'react-select';
-import CustomInsertModal from '../components/custom-insert-modal';
+import React, { Component } from "react";
+import Routes from "../../utils/containers/routes";
+import {
+  Card,
+  Button,
+  Collapse,
+  InputGroup,
+  FormControl
+} from "react-bootstrap";
+import GridNewMainProduct from "../components/grid-new-mainproduct";
+import Select from "react-select";
+import CustomInsertModal from "../components/custom-insert-modal";
 
 class NewSelection extends Component {
+  state = {
+    open1: false,
+    open: false,
+    show: false,
+    items: [],
+    selectOptions: [],
+    tela: [],
+    routes: [
+      { name: "Selecciones", active: false, route: "/selections" },
+      { name: "Nuevo", active: true }
+    ]
+  };
+  selectMulti = e => {
+    this.setState({ selectOptions: e });
+  };
+  saveInfo = e => {
+    const $name_selection = document.getElementById("formNombre").value;
+    const $promedio = parseFloat(document.getElementById("formPromedio").value);
+    const $mano_obra = parseFloat(document.getElementById("formMObra").value);
+    this.state.tela.push({
+      id: "1",
+      name_selection: $name_selection,
+      insumo: this.state.selectOptions.map(element => {
+        let elements = {};
+        elements["promedio"] = $promedio;
+        elements["mano_obra"] = $mano_obra;
+        elements["id_insumo"] = element.value;
+        return elements;
+      })
+    });
 
-    state = {
-        open1: false,
-        open: false,
-        show: false,
-        items: [],
-        selectOptions: [],
-        routes: [{ name: "Selecciones", active: false, route: "/selections" }, { name: "Nuevo", active: true }],
-    }
-    selectMulti = (e) => {
-        this.setState({selectOptions:e})
-        
-    }
-    saveInfo = (e) => {
-        console.log('Guardando Información', document.getElementById("nombreInput"),this.state.selectOptions)
-    }
+    this.setState({
+      tela: this.state.tela,
+      show: false
+    });
+  };
 
-    handleShow = () => {
+  handleShow = () => {
+    this.setState({ show: true });
+  };
 
-        this.setState({ show: true });
-    };
+  handleHide = () => {
+    this.setState({ show: false });
+  };
 
-    handleHide = () => {
-        this.setState({ show: false });
-    };
+  componentWillMount() {
+    fetch("http://localhost:8080/items")
+      .then(response => response.json())
+      .then(items => this.setState({ items }));
+  }
 
-    componentWillMount() {
-        fetch("http://localhost:8080/items")
-            .then(response => response.json())
-            .then(items => this.setState({ items })
-            )
-    }
+  render() {
+    const { open1, open } = this.state;
+    console.log(this.state.items);
 
-    render() {
-        const { open1, open } = this.state
-        console.log(this.state.items);
+    return (
+      <div>
+        <Routes name={this.state.routes} />
+        <Card>
+          <Card.Header>
+            <h5 className="mb-0">
+              <Button
+                aria-expanded={open1}
+                aria-controls="collapseOne"
+                onClick={() => this.setState({ open1: !open1 })}
+                variant="link"
+              >
+                1) Contenido Principal
+              </Button>
+            </h5>
+          </Card.Header>
+          <Collapse in={this.state.open1}>
+            <Card.Body id="collapseOne">
+              <InputGroup size="sm" className="mb-3 cotizacion-inputGroup">
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroup-sizing-sm">
+                    Producto terminado
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  className="inputCantidad"
+                  aria-label="Small"
+                  aria-describedby="inputGroup-sizing-sm"
+                  disabled={this.state.optionVisible}
+                />
+              </InputGroup>
+              <Button variant="secondary" size="sm" onClick={this.handleShow}>
+                Nueva Tela
+              </Button>
+              <GridNewMainProduct tela={this.state.tela} />
+              <CustomInsertModal
+                show={this.state.show}
+                onHide={this.handleHide}
+                items={this.state.items}
+                saveInfo={this.saveInfo}
+                selectMulti={this.selectMulti}
+              />
+            </Card.Body>
+          </Collapse>
+        </Card>
 
-        return (
-            <div>
-
-                <Routes name={this.state.routes} />
-                <Card >
-                    <Card.Header>
-                        <h5 className="mb-0">
-                            <Button
-                                aria-expanded={open1}
-                                aria-controls="collapseOne"
-                                onClick={() => this.setState({ open1: !open1 })}
-                                variant="link"
-                            >
-                                1) Contenido Principal
-                                </Button>
-                        </h5>
-                    </Card.Header>
-                    <Collapse in={this.state.open1}>
-                        <Card.Body id="collapseOne">
-                            <InputGroup size="sm" className="mb-3 cotizacion-inputGroup">
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text id="inputGroup-sizing-sm">Producto terminado</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl className="inputCantidad" aria-label="Small" aria-describedby="inputGroup-sizing-sm" disabled={this.state.optionVisible} />
-                            </InputGroup>
-                            <Button variant="secondary" size="sm" onClick={this.handleShow}>
-                                Nueva Tela
-                            </Button>
-                            <GridNewMainProduct createCustomModal={this.createCustomModal} />
-                            <CustomInsertModal
-                                show={this.state.show}
-                                onHide={this.handleHide}
-                                items={this.state.items}
-                                saveInfo={this.saveInfo} 
-                                selectMulti={this.selectMulti}/>
-                        </Card.Body>
-                    </Collapse>
-                </Card>
-
-                <Card >
-                    <Card.Header>
-                        <h5 className="mb-0">
-                            <Button
-                                aria-expanded={open}
-                                aria-controls="collapseOne"
-                                onClick={() => this.setState({ open: !open })}
-                                variant="link"
-                            >
-                                1) Contenido Principal
-                                </Button>
-                        </h5>
-                    </Card.Header>
-                    <Collapse in={this.state.open}>
-                        <Card.Body id="collapseOne">
-                            <InputGroup size="sm" className="mb-3 cotizacion-inputGroup">
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text id="inputGroup-sizing-sm">Producto terminado</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl className="inputCantidad" aria-label="Small" aria-describedby="inputGroup-sizing-sm" disabled={this.state.optionVisible} />
-                            </InputGroup>
-                            <InputGroup size="sm" className="mb-3 cotizacion-inputGroup">
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text id="inputGroup-sizing-sm">Tela</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl className="inputCantidad" aria-label="Small" aria-describedby="inputGroup-sizing-sm" disabled={this.state.optionVisible} />
-                            </InputGroup>
-                            <Select
-
-                                placeholder="Seleccione...."
-                                options={[{ label: "Insumo 1", value: "1" }, { label: "Insumo 2", value: "2" }]
-
-                                }
-
-
-                            />
-                            <InputGroup className="mb-3 cotizacion-inputGroup">
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text id="inputGroup-sizing-sm">Insumos</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <Select
-
-                                    placeholder="Seleccione...."
-                                    options={[{ label: "Insumo 1", value: "1" }, { label: "Insumo 2", value: "2" }]
-
-                                    }
-
-
-                                />
-                            </InputGroup>
-                        </Card.Body>
-                    </Collapse>
-                </Card>
-            </div>
-
-        )
-    }
+        <Card>
+          <Card.Header>
+            <h5 className="mb-0">
+              <Button
+                aria-expanded={open}
+                aria-controls="collapseOne"
+                onClick={() => this.setState({ open: !open })}
+                variant="link"
+              >
+                1) Contenido Principal
+              </Button>
+            </h5>
+          </Card.Header>
+          <Collapse in={this.state.open}>
+            <Card.Body id="collapseOne">
+              <InputGroup size="sm" className="mb-3 cotizacion-inputGroup">
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroup-sizing-sm">
+                    Producto terminado
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  className="inputCantidad"
+                  aria-label="Small"
+                  aria-describedby="inputGroup-sizing-sm"
+                  disabled={this.state.optionVisible}
+                />
+              </InputGroup>
+              <InputGroup size="sm" className="mb-3 cotizacion-inputGroup">
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroup-sizing-sm">
+                    Tela
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  className="inputCantidad"
+                  aria-label="Small"
+                  aria-describedby="inputGroup-sizing-sm"
+                  disabled={this.state.optionVisible}
+                />
+              </InputGroup>
+              <Select
+                placeholder="Seleccione...."
+                options={[
+                  { label: "Insumo 1", value: "1" },
+                  { label: "Insumo 2", value: "2" }
+                ]}
+              />
+              <InputGroup className="mb-3 cotizacion-inputGroup">
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroup-sizing-sm">
+                    Insumos
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <Select
+                  placeholder="Seleccione...."
+                  options={[
+                    { label: "Insumo 1", value: "1" },
+                    { label: "Insumo 2", value: "2" }
+                  ]}
+                />
+              </InputGroup>
+            </Card.Body>
+          </Collapse>
+        </Card>
+      </div>
+    );
+  }
 }
-export default NewSelection
+export default NewSelection;
